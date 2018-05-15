48da1e68528c292802449765d4f4213c3b440ae7
Vanderson <vando.zc@gmail.com>
2018-05-14 23:54:27 -0300

    Correção de merge

--- src/main/java/com/vandersoncamp/ampereweb/util/JaxRsActivator.java
+++ src/main/java/com/vandersoncamp/ampereweb/util/JaxRsActivator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.vandersoncamp.ampereweb.util;
 
 import javax.ws.rs.ApplicationPath;
@@ -8,14 +7,3 @@ import javax.ws.rs.core.Application;
 public class JaxRsActivator extends Application {
     
 }
-=======
-package com.vandersoncamp.ampereweb.util;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JaxRsActivator extends Application {
-    
-}
->>>>>>> cbaa76bc79c17be56987a753058da61eebd31def
