9cff08f41d60d74102bf074a98fc75f87eec193d
Vanderson <vando.zc@gmail.com>
2018-06-02 00:26:12 -0300

    Front config

--- src/main/webapp/app/module.js
+++ src/main/webapp/app/module.js
@@ -9,38 +9,37 @@
   function AppConfig($stateProvider) {
 
     $stateProvider
-
-
+    
       .state({
-        name: 'AparelhosList',
+        name: 'aparelhosList',
         url: '/aparelhos',
         templateUrl: '/app/aparelhos/aparelhos-list/aparelho-list.html',
         controller: 'AparelhoListController',
         controllerAs: 'vm'
       })
       .state({
-        name: 'AparelhosNovo',
+        name: 'aparelhosNovo',
         url: '/aparelhos/novo',
         templateUrl: '/app/aparelhos/aparelhos-cad/aparelho-cad.html',
         controller: 'AparelhoCadController',
         controllerAs: 'vm'
       })
       .state({
-        name: 'AparelhosEditar',
+        name: 'aparelhosEditar',
         url: '/aparelhos/{id}',
         templateUrl: '/app/aparelhos/aparelhos-cad/aparelho-cad.html',
         controller: 'AparelhoCadController',
         controllerAs: 'vm'
       })
       .state({
-        name: 'CondutorList',
+        name: 'condutorList',
         url: '/condutores',
         templateUrl: '/app/condutores/condutores-list/condutor-list.html',
         controller: 'CondutorListController',
         controllerAs: 'vm'
       })
       .state({
-        name: 'CondutorNovo',
+        name: 'condutorNovo',
         url: '/condutores/novo',
         templateUrl: '/app/condutores/condutores-cad/condutor-cad.html',
         controller: 'CondutorCadController',
