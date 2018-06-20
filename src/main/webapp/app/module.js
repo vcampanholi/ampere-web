33a2cd2fb119e10a93d726023c9feafa94bf0ee1
Vanderson Campanholi <vando.zc@gmail.com>
2018-06-20 01:02:10 -0300

    angular-input-mask dependencies

--- src/main/webapp/app/module.js
+++ src/main/webapp/app/module.js
@@ -1,5 +1,5 @@
 (function () {
-  angular.module('app', ['ui.router']);
+  angular.module('app', ['ui.router', 'ui.utils.masks']);
 
   angular.module('app')
     .config(AppConfig);
@@ -9,7 +9,6 @@
   function AppConfig($stateProvider) {
 
     $stateProvider
-    
       .state({
         name: 'usuarioList',
         url: '/usuarios',
